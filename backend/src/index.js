21ac382c787e62d3115ce9131b1a8c02094e6d67
Peter Durham <peterdurham@protonmail.com>
2020-03-28 11:09:00 -0700

    yay

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
@@ -27,18 +27,18 @@ server.express.use(async (req, res, next) => {
   if (!req.userId) return next();
   const user = await db.query.user(
     { where: { id: req.userId } },
-    '{ id, permissions, email, name }'
+    "{ id, permissions, email, name }"
   );
   req.user = user;
   next();
 });
-
+// start it!
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
+      origin: process.env.FRONTEND_URL
+    }
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
