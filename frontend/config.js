9b4692068f92084cdbeda64fcf4e22163776af7d
Peter Durham <peterdurham@protonmail.com>
2020-04-05 20:38:59 -0700

    updated per page to 12

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint = `https://culture-club-yoga-prod.herokuapp.com/`;
-export const perPage = 8;
+export const perPage = 12;
 
 export const MovieGenres = [
   {
