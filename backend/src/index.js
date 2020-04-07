e3cdba404fdc6ff23719a43c337e0c0a53f25f2a
Peter Durham <peterdurham@protonmail.com>
2020-04-06 17:47:36 -0700

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,6 +33,16 @@ server.express.use(async (req, res, next) => {
   next();
 });
 // start it!
+
+server.express.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:7777");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.start(
   {
     cors: {
